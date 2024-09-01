1a7d736e4ec60fd70ddeea04fd5e0e7578228de5
Sarthak <sarthakhingankar@gmail.com>
2024-09-01 14:52:59 +0530

    init todo again lol

--- C/todo.c
+++ C/todo.c
@@ -1,105 +1,48 @@
 #include <stdio.h>
-#include <string.h>
-
-
-const int MAX_Tasks = 10;
-const int MAX_Tasklength = 30;
-
-void addTask(char tasks[MAX_Tasks][MAX_Tasklength]){
-    printf("What tasks would you like to add?\n");
-    char task[30];
-    scanf("%s", &task);
-    for (int i = 0; i < MAX_Tasks; i++){
-        if (strcmp(tasks[i], "") == 0) {
-            strcpy(tasks[i], task);
-            break;
-        }
-    }
-    printf("Task added successfully!\n");
-    printf("Press enter to continue");
-    scanf("%c");
-}
-
-void deleteTask(){
-
-}
-
-void completeTask(){
-
-}
-
-void viewTasks(char tasks[MAX_Tasks][MAX_Tasklength]){
-    printf("Your Tasks are:\n");
-    for (int i = 0; i < MAX_Tasks; i++) {
-        if (strcmp(tasks[i], "")!= 0) {
-            printf("(%d) %s\n", i + 1, tasks[i]);
-        }
-    }
-    printf("Press enter to continue");
-    scanf("%c");
-}
-
-void viewCompletedTasks(){
-
-}
-
-void editTask(){
-
-}
 
 int main(int argc, char const *argv[])
 {
+    int choice;
+    int running = 1;
 
-    char tasks[MAX_Tasks][MAX_Tasklength];
-    for (int i = 0; i < MAX_Tasks; i++) {
-        strcpy(tasks[i], "");
-    }
-    
-
-    printf("Hey there! It's Your personal todo list\n");
-
-    while (1)
+    while (running)
     {
-        printf("What would you like to do?\n");
-        printf("Add a task (1)\n");
-        printf("Delete a task (2)\n");
-        printf("Complete a task (3)\n");
-        printf("Display all tasks (4)\n");
-        printf("Edit a task (5)\n");
-        printf("Display completed tasks (6)\n");
-        printf("Exit (7)\n");
 
-        int choice;
+        printf("Welcome to your very own todo list!\n");
+        printf("Enter (1) to Add Task\n"
+               "Enter (2) to View Task\n"
+               "Enter (3) to Complete Task\n"
+               "Enter (4) to Delete Task\n"
+               "Enter (5) to Edit Task\n"
+               "Enter (6) to Exit\n");
+
+        printf("Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice)
         {
         case 1:
-            addTask(tasks);
+            printf("What task would you like to add?");
             break;
         case 2:
-            deleteTask();
+            printf("2");
             break;
         case 3:
-            completeTask();
+            printf("3");
             break;
         case 4:
-            viewTasks(tasks);
+            printf("4");
             break;
         case 5:
-            editTask();
+            printf("5");
             break;
         case 6:
-            viewCompletedTasks();
+            printf("6");
             break;
-        case 7:
-            printf("Goodbye! Have a great day!\n");
-            return 0;
         default:
             printf("Invalid choice. Please try again.\n");
-            return 0;
+            break;
         }
     }
-    
     return 0;
 }
